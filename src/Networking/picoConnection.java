163d2fea359199a4c4bf7de256b9c89e0188a5e6
Rayan Diya <yanodiya@gmail.com>
2026-05-09 07:06:20 +0300

    Finished the actual connection

--- src/Networking/picoConnection.java
+++ src/Networking/picoConnection.java
@@ -1,21 +1,21 @@
 package Networking;
 
-import java.io.IOError;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Queue;
 
 public class picoConnection {
     private Socket picoSocket;
 
-    public picoConnection(int port, String IP) {
+    public picoConnection(int port, String IP, Queue<picoPacket> picoPackets) {
         try {
             this.picoSocket = new Socket(IP, port);
+
+            Runnable piRunnable = new PiCRXRun(picoSocket, picoPackets);
+            Thread RXThread = new Thread(piRunnable);
+            RXThread.start();
         } catch (IOException e) {
             System.out.println("Pico connection failed");
         }
-    }
-
-    private void openSocketReader() {
-        Thread RXThread = new Thread();
     }
 }
